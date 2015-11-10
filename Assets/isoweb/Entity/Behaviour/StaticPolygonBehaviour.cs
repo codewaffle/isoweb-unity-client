93c93ba4e07a033b61c4df4509414b55275ac929
Patrick Griffus <patrick@codewaffle.com>
2015-11-09 23:41:50 -0500

    remder a big pink island

--- Assets/isoweb/Entity/Behaviour/StaticPolygonBehaviour.cs
+++ Assets/isoweb/Entity/Behaviour/StaticPolygonBehaviour.cs
@@ -1,24 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 using SimpleJSON;
 
-public class StaticPolygonBehaviour : MonoBehaviour {
+public class StaticPolygonBehaviour : MonoBehaviour
+{
+    private Mesh _mesh;
+    private MeshRenderer _renderer;
+    private MeshFilter _meshFilter;
 
-	// Use this for initialization
-	void Start () {
-	
+    void Awake()
+	{
+	    _renderer = gameObject.AddComponent<MeshRenderer>();
+	    _meshFilter = gameObject.AddComponent<MeshFilter>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public void SetTextureUrl(string url)
     {
     }
 
     public void SetPoints(Vector2[] points)
     {
+        var tri = new Triangulator(points);
+        var indices = tri.Triangulate();
+
+        if (_mesh == null)
+            _mesh = new Mesh();
+
+        _mesh.Clear();
+        _mesh.vertices = points.Select(s => (Vector3) s).ToArray();
+        _mesh.uv = points;
+        _mesh.triangles = indices;
+        _meshFilter.mesh = _mesh;
     }
 }
